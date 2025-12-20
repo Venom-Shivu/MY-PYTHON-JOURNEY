b25f7285feebed4e3fe1f39140e3497760156afe
Shivansh Yadav <mrshivusinghyadav@gmail.com>
2025-12-20 11:55:33 +0530

    Inital commit

--- Chapter 2 (variables & datatypes)/02data_types.py
+++ Chapter 2 (variables & datatypes)/02data_types.py
@@ -1,5 +1,15 @@
 a=1 # a is a integer
 b=5.223 # b is a floating pint number
 c="Shivu" # c is string
-d= False # d is a string
-e= None # eis a none type variable
+d= False # d is a boolean
+e= None # eis a none type 
+f= 123456789123456789 # f is a long integer
+g= 1+2j # g is a complex number
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(f))
+print(type(e))
+print(type(g))
+
